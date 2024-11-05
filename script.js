6f7543c91a92b6075ebed68d4c21725b8a3c18a7
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2024-11-05 16:08:07 +0000

    Update

---
+++ script.js
@@ -0,0 +1,34 @@
+let container2 = document.getElementsByClassName("container2")[0];
+let container3 = document.getElementsByClassName("container3")[0];
+let checkIcon = document.getElementById("check-icon");
+let xIcon = document.getElementById("x-icon");
+let i = 0;
+
+xIcon.addEventListener("click", function(){
+    createNote();
+});
+
+checkIcon.addEventListener("click", function(){
+    createNote();
+});
+
+function typeNote(){
+
+}
+
+function createNote(){
+
+}
+
+function margin(){
+    let random_margin = ["-5px", "1px", "5px","10px","15px", "20px"];
+}
+
+function rotate(){
+
+
+}
+
+function color(){
+
+}
